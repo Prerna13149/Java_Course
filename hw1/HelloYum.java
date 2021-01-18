d5a858b4980c2d98831b120ba5badc0192865181
prerna13149 <prerna13149@gmail.com>
2021-01-18 09:55:50 -0500

    hello world

---
+++ hw1/HelloYum.java
@@ -0,0 +1,16 @@
+/*
+ * Author : Prerna Singh
+ * Andrew Id :  prernasi
+ */
+public class HelloYum {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Andrew ID: prernasi");
+        System.out.println("First Name: Prerna");
+        System.out.println("Last Name: Singh");
+        System.out.println("Favorite Restaurant: Indian Masala House Bistro");
+        System.out.println("Best Dish: Kadhai Paneer");
+    }
+
+}
